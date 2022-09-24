002a41ea7fc814b8aea7f2a5e0729828bf9b2413
Karelys Amador <karelysamador95@gmail.com>
2022-09-24 12:55:16 +0200

    ejercicio 1 y 2

--- week-2/Homework/Part-2/B-objects-get-set/exercise-1.js
+++ week-2/Homework/Part-2/B-objects-get-set/exercise-1.js
@@ -2,11 +2,12 @@
     Console.log the values of each property of "kitten"
 */
 
-var kitten = {
+let kitten = {
     ageMonths: 3,
     isFemale: true,
     furColour: "brown"
 };
 
 // YOUR CODE HERE
-
+const json=JSON.stringify(kitten, null, 2)
+console.log(json);
