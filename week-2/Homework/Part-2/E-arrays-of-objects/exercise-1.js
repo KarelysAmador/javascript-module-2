0f21a7d599f2911690255e7299e344e08e0c8b04
Karelys Amador <karelysamador95@gmail.com>
2022-09-27 19:53:08 +0200

    ejercicio e, 1 y 2

--- week-2/Homework/Part-2/E-arrays-of-objects/exercise-1.js
+++ week-2/Homework/Part-2/E-arrays-of-objects/exercise-1.js
@@ -24,12 +24,13 @@ DO NOT EDIT ANYTHING ABOVE THIS LINE
 WRITE YOUR CODE BELOW
 */
 
+let persons = [person1, person2, person3];
+// Complete here
 
-let persons = // Complete here
+let personNames = persons.map ((person)=> person.name );
+ // Complete here
 
-let personNames = // Complete here
-
-let personsYoungerThan28YearsOld = // Complete here
+let personsYoungerThan28YearsOld = persons.filter ((person) => person.age <= 28 );
 
 
 /*
