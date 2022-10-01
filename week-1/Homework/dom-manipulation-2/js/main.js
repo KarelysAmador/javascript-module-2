dea230c7bf7557665a244261ea9530e8935102ac
Karelys Amador <karelysamador95@gmail.com>
2022-10-01 11:54:49 +0200

    DOM 2 terminado

--- week-1/Homework/dom-manipulation-2/js/main.js
+++ week-1/Homework/dom-manipulation-2/js/main.js
@@ -1,144 +1,59 @@
-/*
-Clicking on the buttons should change the "theme" of the website:
+const jumButtom = document.querySelector('.jumbotron')
+const primaryButtom = document.querySelector('.btn-lrg')
+const secundaryButtom = document.querySelector('.btn-secondary')
+console.log(primaryButtom);
 
-- When clicking **blue** it should change:
+const blueButton = document.querySelector('#blueBtn')
+blueButton.addEventListener('click',()=>{
+    jumButtom.style.backgroundColor = '#588fbd'
+    primaryButtom.style.backgroundColor = '#ffa500'
+    secundaryButtom.style.backgroundColor = 'black'
+    secundaryButtom.style.color = 'white'
+} )
 
-  - **Jumbotron** background color to `#588fbd`
-  - **Donate a bike** button background color to `#ffa500`
-  - **Volunteer** button background color to `black` and text color to `white`
-*/
-    constjumbotronEl=document.querySelector(".jumbotron");
-    constbtnBlueEl=document.querySelector("#blueBtn");
-    constbtnPrimaryEl=document.querySelector("a.btn.btn-primary.btn-lrg");
-    constbtnVolunteerEl=document.querySelector("a.btn.btn-secondary.btn-lrg");
-    btnBlueEl.addEventListener("click",e=>{
-        e.preventDefault();
-        jumbotronEl.style.background="#588fbd";
-        btnPrimaryEl.style.background="#ffa500";
-        btnVolunteerEl.style.background="black";
-        btnVolunteerEl.style.color="white";
-    })
+const orangeButton = document.querySelector('#orangeBtn')
+orangeButton.addEventListener('click',()=>{
+    jumButtom.style.backgroundColor = '#f0ad4e'
+    primaryButtom.style.backgroundColor = '#5751fd'
+    secundaryButtom.style.backgroundColor = '#31b0d5'
+    secundaryButtom.style.color = 'white'
+} )
 
-/*
-- When clicking **orange** it should change:
+const greenButton = document.querySelector('#greenBtn')
+greenButton.addEventListener('click',()=>{
+    jumButtom.style.backgroundColor = '#87ca8a'
+    primaryButtom.style.backgroundColor = 'black'
+    secundaryButtom.style.backgroundColor = '#8c9c08'
+} )
 
-  - **Jumbotron** background color to `#f0ad4e`
-  - **Donate a bike** button background color to `#5751fd`
-  - **Volunteer** button background color to `#31b0d5` and text color to `white`
-*/
-    constbtnOrangeEl=document.querySelector("#orangeBtn");
-    btnOrangeEl.addEventListener("click",e=>{
-        e.preventDefault();
-        jumbotronEl.style.background="#f0ad4e";
-        btnPrimaryEl.style.background="#5751fd";
-        btnVolunteerEl.style.background="#31b0d5";
-        btnVolunteerEl.style.color="white";
-    })
-/*
-- When clicking **green** it should change:
-  - **Jumbotron** background color to `#87ca8a`
-  - **Donate a bike** button background color to `black`
-  - **Volunteer** button background color to `#8c9c08`
+const formulario = document.querySelector('form')
 
-Here's an example of how the website should look for the blue button:
+const emailInput= document.querySelector('#exampleInputEmail1')
+const nameInput= document.querySelector('#example-text-input')
+const descriptionInput= document.querySelector('#exampleTextarea')
+const verificarInputs = (input) => input.value.length == 0  
 
-[Blue button example](images/blue_clicked.png)
-*/
-constbtnGreenEl=document.querySelector("#greenBtn");
-btnGreenEl.addEventListener("click",e=>{
-    e.preventDefault();
-    jumbotronEl.style.background="#87ca8a";
-    btnPrimaryEl.style.background="black";
-    btnVolunteerEl.style.background="#8c9c08";
-})
-/*
-## Part 2
+formulario.addEventListener('submit',(e) => {
+    e.preventDefault()
 
-Just below the buttons, there's a form called **Register with us**.
+   
+    if (!verificarInputs(emailInput) && !verificarInputs(nameInput) && !verificarInputs(descriptionInput) && emailInput.value.includes('@')) {
+      const inputs = [emailInput,nameInput,descriptionInput]
+       inputs.forEach((input)=> {
+           input.style.backgroundColor = 'white'
+           input.value = ''
+       } ) 
+        alert('thank you for filling out the form')
 
-Continue working in `./js/main.js` to add the following functionality:
-
-When the submit button is pressed, it should check that all the form fields are valid:
-
-- The **Your name**, **Email** and **Describe yourself** fields need to be non-empty (Hint: their `value` length has to be greater than zero)
-- For the **Email** field also check if it contains the `@` character
-
-For all the fields that invalid, it should make their background color `red`.
-IF all the fields are valid, when you click **Submit** it should:
-
-- Display an alert to thank you for filling out the form
-- Blank out (make empty) all the text fields
-
-**Important hint:** In your function that handles clicks on the `Submit` button you will need to call `event.preventDefault()` to stop the browser from refreshing the page. To read more on how to do this: https://developer.mozilla.org/en/docs/Web/API/Event/preventDefault
-
-*/
-
-constbtnSubmitEl=document.querySelector("form button.btn.btn-primary");
-constnameEl=document.querySelector("#example-text-input");
-constemailEl=document.querySelector("#exampleInputEmail1");
-constdescribeEl=document.querySelector("#exampleTextarea");
-
-functionnonEmpty(text) 
-{
-    console.log(text,text.length);
-    returntext.length>0;
-}
-
-functionmarkColor(element,color)
-
-{
-    console.log("validating..",element);
-    element.style.background=color;
-}
-
-functionblankOut(elements) 
-{
-    elements.forEach(element=>{
-        element.textContent="";
-        markColor(element,"white");
-        
-    });
-    
-}
-
-functionvalidate(elements,validator,coloring)
-{
-    letvalid=true;
-    elements.forEach(element=>{
-        if(!validator(element.value)){
-            console.log(element,"EMPTY");
-            valid=false;
-            coloring(element,"red");
-        }
-    });
-    returnvalid;
-    
-        
-    
-}
-
-btnSubmitEl.addEventListener("click",e=>{
-    e.preventDefault();
-    console.log("click botton");
-    constelementsToValidate=[nameEl,emailEl,describeEl];
-    console.log("elementsToValidate",elementsToValidate);
-    constvalid=validate(elementsToValidate,nonEmpty,markColor);
-    console.log("valid",valid);
-    if(valid){
-    blankOut(elementsToValidate);
-
-}
-    if(!emailEl.value.includes("@")){
-        
-        console.log("bad email");
-        markColor(emailEl,"red");
-    }else{
-        if(valid){
-            alert("Thank you for filling out the form");
-        }
-        
+    } else {
+    if (verificarInputs(emailInput) || !emailInput.value.includes('@')) {
+        emailInput.style.backgroundColor = 'red'
     }
-
+    if (verificarInputs(nameInput)) {
+        nameInput.style.backgroundColor = 'red'
+    }
+    if (verificarInputs(descriptionInput)) {
+        descriptionInput.style.backgroundColor = 'red'
+    }
+}
 })
-
-
